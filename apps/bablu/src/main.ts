9f4d79f0b44481015a6d8e7cfe140829c4353c2c
MohitR1999 <mohitr1999@gmail.com>
2026-01-30 00:09:49 +0530

    added main controlling file

--- apps/bablu/src/main.ts
+++ apps/bablu/src/main.ts
@@ -1,4 +1,13 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+import { makeBot } from "./app/makeBot";
 
+dotenv.config();
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const token = process.env.DISCORD_TOKEN;
 
+if (!token) {
+  console.error(`Discord bot token not found!`);
+} else {
+  const controller = makeBot(token);
+  controller.start();
+}
