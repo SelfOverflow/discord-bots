4dfae7f1740c84f2ead30115cd6dabff4c41c04b
Mohit Ranjan <mohitr1999@gmail.com>
2026-02-28 20:16:14 +0530

    Deploy ECS (#28)
    * added ecs
    
    * corrected dockerfile
    
    * fixed tests

--- apps/bablu/__tests__/sanity.test.ts
+++ apps/bablu/__tests__/sanity.test.ts
@@ -1,23 +1,12 @@
-import { handler } from "../src";
-import { APIGatewayEvent, Context } from "aws-lambda";
-
+import { handler } from '../src/index'
 describe("Basic handler", () => {
   afterEach(() => {
     jest.restoreAllMocks();
   });
 
   it("Should return basic status 200 code", async () => {
-    const event: unknown = {};
-    const context: unknown = {};
-    const returnObject = await handler(
-      event as APIGatewayEvent,
-      context as Context,
-    );
-    expect(returnObject.statusCode).toBe(200);
-    expect(returnObject.body).toBe(
-      JSON.stringify({
-        message: "Hello World",
-      }),
-    );
+    const spyLog = jest.spyOn(console, 'log');
+    handler()
+    expect(spyLog).toHaveBeenCalledWith('Hello World')
   });
 });
