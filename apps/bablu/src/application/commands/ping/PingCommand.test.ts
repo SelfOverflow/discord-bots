e57193d5bc04ebe94ba6845c4b058dfdd6ead3aa
MohitR1999 <mohitr1999@gmail.com>
2026-03-01 14:45:23 +0530

    fixed testcase

--- apps/bablu/src/application/commands/ping/PingCommand.test.ts
+++ apps/bablu/src/application/commands/ping/PingCommand.test.ts
@@ -4,6 +4,6 @@ describe('PingCommandHandler', () => {
     it('Should return Pong!', async () => {
         const handler = new PingCommandHandler()
         const response = await handler.execute();
-        expect(response).toBe('Pong!')
+        expect(response).toBe('Pong baby!')
     })
 })
