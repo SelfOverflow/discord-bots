11c8771ed620874ed470a50d2b0febfe0e7e9d6f
MohitR1999 <mohitr1999@gmail.com>
2026-03-09 21:38:48 +0530

    changed secret name

--- apps/bablu/terraform/secrets.tf
+++ apps/bablu/terraform/secrets.tf
@@ -1,9 +1,9 @@
 resource "aws_secretsmanager_secret" "discord_token" {
-  name = "${var.project_name}-discord-token"
+  name = "${var.project_name}-discord-app-token"
   tags = local.common_tags
 }
 
 resource "aws_secretsmanager_secret" "app_id" {
-  name = "${var.project_name}-app-id"
+  name = "${var.project_name}-bot-client-id"
   tags = local.common_tags
 }
