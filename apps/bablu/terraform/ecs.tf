1f38f8faeccc2b218cedf45ec573b7e9d1f5da12
MohitR1999 <mohitr1999@gmail.com>
2026-03-20 19:28:29 +0530

    Added ECS cluster capacity provider

--- apps/bablu/terraform/ecs.tf
+++ apps/bablu/terraform/ecs.tf
@@ -50,3 +50,30 @@ resource "aws_autoscaling_group" "ecs" {
     propagate_at_launch = true
   }
 }
+
+resource "aws_ecs_capacity_provider" "main" {
+  name = "${var.project_name}-ecs-capacity-provider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
+    managed_termination_protection = "DISABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 2
+      minimum_scaling_step_size = 2
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
+  }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+  cluster_name       = aws_ecs_cluster.bablu-ecs-cluster.name
+  capacity_providers = [aws_ecs_capacity_provider.main.name]
+
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.main.name
+    base              = 1
+    weight            = 100
+  }
+}
