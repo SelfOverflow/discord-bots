4b4f4f96d66af1ebc67ebc9ecbc022c6b0f10935
MohitR1999 <mohitr1999@gmail.com>
2026-03-24 00:48:38 +0530

    removed uppercase character

--- apps/bablu/src/application/commands/addBirthday/AddBirthdayCommand.ts
+++ apps/bablu/src/application/commands/addBirthday/AddBirthdayCommand.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from 'discord.js'
 import { SlashCommandDefinition } from '../../../domain/SlashCommandDefinition'
 
 export class AddBirthdayCommand implements SlashCommandDefinition {
-    name = "addBirthday"
+    name = "addbirthday"
     description = "Allows you to set your birthday in the server"
     build(): SlashCommandBuilder {
         return new SlashCommandBuilder().setName(this.name).setDescription(this.description)
